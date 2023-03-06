27fc1537dfdaecde50c42416a9b4ffc2f93e320c
GervinLuide <91869560+GervinLuide@users.noreply.github.com>
2023-03-06 12:56:13 +0200

    Add files via upload
    As a user I can sign up

--- index.js
+++ index.js
@@ -1,47 +1,158 @@
 const express = require('express');
+const cookieParser = require("cookie-parser");
+const sessions = require('express-session');
+const http = require('http');
+var parseUrl = require('body-parser');
 const app = express();
-const swaggerUi = require('swagger-ui-express');
-const yamlJs = require('yamljs');
-const swaggerDocument = yamlJs.load('./swagger.yaml');
 
-require('dotenv').config();
+var mysql = require('mysql');
+const { encode } = require('punycode');
 
-const port = process.env.PORT || 3000;
+let encodeUrl = parseUrl.urlencoded({ extended: false });
 
-// Serve static files
-app.use(express.static('public'));
+//session middleware
+app.use(sessions({
+    secret: "thisismysecrctekey",
+    saveUninitialized:true,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 }, // 24 hours
+    resave: false
+}));
 
-// Use the Swagger UI
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(cookieParser());
 
-// Middleware to parse JSON
-app.use(express.json());
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root", // my username
+    password: "qwerty", // my password
+    database: "login"
+});
 
-// General error handler
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    const status = err.statusCode || 500;
-    res.status(status).send(err.message);
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/signup.html');
 })
 
-// Handle the signup form submission
-app.post('/users', (req, res) => {
-    // Get the form data from the request body
-    const {username, email, password, confirm_password} = req.body;
+app.post('/signup', encodeUrl, (req, res) => {
+    var firstName = req.body.firstName;
+    var lastName = req.body.lastName;
+    var userName = req.body.userName;
+    var password = req.body.password;
 
-    // Check if the password and confirmation password match
-    if (password !== confirm_password) {
-        res.status(400).send('Passwords do not match');
-        return;
-    }
+    con.connect(function(err) {
+        if (err){
+            console.log(err);
+        };
+        // checking user already registered or no
+        con.query(`SELECT * FROM users WHERE username = '${userName}'`, function(err, result){
+            if(err){
+                console.log(err);
+            };
+            if(Object.keys(result).length > 0){
+                res.sendFile(__dirname + '/failreg.html');
+            }else{
+                //creating user page in userPage function
+                function userPage(){
+                    // We create a session for the dashboard (user page) page and save the user data to this session:
+                    req.session.user = {
+                        firstname: firstName,
+                        lastname: lastName,
+                        username: userName,
+                        password: password
+                    };
 
-    // TODO: Check if the username and email are available and meet any other criteria you have for user accounts
+                    res.send(`
+                <!DOCTYPE html>
+                <html lang="en">
+                <head>
+                    <title>Login</title>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1">
+                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+                </head>
+                <body>
+                    <div class="container">
+                        <h3>Hi, ${req.session.user.firstname} ${req.session.user.lastname}</h3>
+                        <a href="/">Log out</a>
+                    </div>
+                </body>
+                </html>
+                `);
+                }
+                // inserting new user data
+                var sql = `INSERT INTO users (firstname, lastname, username, password) VALUES ('${firstName}', '${lastName}', '${userName}', '${password}')`;
+                con.query(sql, function (err, result) {
+                    if (err){
+                        console.log(err);
+                    }else{
+                        // using userPage function for creating user page
+                        userPage();
+                    };
+                });
 
-    // If everything is valid, create the new user account
-    // TODO: Write code to create a new user account
+            }
 
-    res.send('Thanks for signing up!');
+        });
+    });
+
+
 });
+
+app.get("/login", (req, res)=>{
+    res.sendFile(__dirname + "/login.html");
+});
+
+app.post("/dashboard", encodeUrl, (req, res)=>{
+    var userName = req.body.userName;
+    var password = req.body.password;
+
+    con.connect(function(err) {
+        if(err){
+            console.log(err);
+        };
+        con.query(`SELECT * FROM users WHERE username = '${userName}' AND password = '${password}'`, function (err, result) {
+            if(err){
+                console.log(err);
+            };
+
+            function userPage(){
+                // We create a session for the dashboard (user page) page and save the user data to this session:
+                req.session.user = {
+                    firstname: result[0].firstname,
+                    lastname: result[0].lastname,
+                    username: userName,
+                    password: password
+                };
+
+                res.send(`
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <title>Login</title>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1">
+                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+            </head>
+            <body>
+                <div class="container">
+                    <h3>Hi, ${req.session.user.firstname} ${req.session.user.lastname}</h3>
+                    <a href="/">Log out</a>
+                </div>
+            </body>
+            </html>
+            `);
+            }
+
+            if(Object.keys(result).length > 0){
+                userPage();
+            }else{
+                res.sendFile(__dirname + '/faillog.html');
+            }
+
+        });
+    });
+});
+
+
+const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
     console.log(`App running. Docs at http://localhost:${port}/docs`);
