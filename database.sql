c1802699abfeccf031eb1b659db2ad56d909eed7
Robin <robinnoormets@gmail.com>
2023-10-10 12:39:24 +0300

    Delete all files

--- database.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE users
-(
-    id        INT PRIMARY KEY AUTO_INCREMENT,
-    firstname VARCHAR(50)  NOT NULL,
-    lastname  VARCHAR(50)  NOT NULL,
-    username  VARCHAR(50)  NOT NULL,
-    password  VARCHAR(100) NOT NULL
-);
